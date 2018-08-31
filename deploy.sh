e1d1f4d3973eea509408190546df3679128f19dc
Pedro Bruel <pedro.bruel@gmail.com>
2018-08-31 16:12:28 +0200

    Fri Aug 31 16:12:28 CEST 2018

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
-echo "Setting up ssh-agent and keys"
-echo "Remember to set ForwardAgent to yes"
-
-eval $(ssh-agent)
-ssh-add
-ssh-add -L
-
 echo "Testing connectivity to GitHub"
 ssh -T git@github.com
 
@@ -14,6 +7,13 @@ echo "Allocating machines on 'grisou'"
 
 oarsub -p "cluster='grisou'" -l nodes=1,walltime=2 -I -t deploy
 
-echo "Deploying debian9-x64-big image"
+echo "Deploying orio_experiments image"
 
 kadeploy3 -f $OAR_NODE_FILE -a orio_experiments.env -k
+
+echo "Setting up ssh-agent and keys"
+echo "Remember to set ForwardAgent to yes"
+
+eval $(ssh-agent)
+ssh-add
+ssh-add -L
