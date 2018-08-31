e98fd564fdec56347bda15ea601d82cda3a6e9ab
Pedro Bruel <pedro.bruel@gmail.com>
2018-08-31 22:39:39 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 #! /bin/bash
 
-echo "Testing connectivity to GitHub"
-ssh -T git@github.com
-
 echo "Allocating machines on 'grisou'"
 
-oarsub -p "cluster='grisou'" -l nodes=1,walltime=2 -I -t deploy
+oarsub -q production -p "cluster='graoully'" -l nodes=4,walltime=3 -I -t deploy
 
 echo "Deploying orio_experiments image"
 
-kadeploy3 -f $OAR_NODE_FILE -a orio_experiments.env -k
+kadeploy3 -f $OAR_NODE_FILE -a /home/phrb/orio_experiments.env -k
 
 echo "Setting up ssh-agent and keys"
 echo "Remember to set ForwardAgent to yes"
@@ -17,3 +14,7 @@ echo "Remember to set ForwardAgent to yes"
 eval $(ssh-agent)
 ssh-add
 ssh-add -L
+
+echo "Testing connectivity to GitHub"
+ssh -T git@github.com
+
