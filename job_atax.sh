8a84c3c3faf35b5fcbda42707ebe530fdc426fd9
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-03 20:46:22 +0200

    Fix variable names

--- job_atax.sh
+++ job_atax.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-USR="pbruel"
-APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/atax"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
-NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-
 apt-get install -y python-rpy2 r-cran-car
 Rscript -e 'install.packages("AlgDesign", repos="https://cran.rstudio.com")'
 pip install dataset
@@ -15,6 +10,11 @@ cd $APP_TARGET
 
 #./run_multiple.sh 1 atax2.src1.c
 ./run_multiple.sh 1 atax2.src1_random.c
+
+USR="pbruel"
+APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/atax"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
--- job_stencil3d.sh
+++ job_stencil3d.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-USR="pbruel"
-APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/stencil3d"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/stencil3d"
-NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-
 apt-get install -y python-rpy2 r-cran-car
 Rscript -e 'install.packages("AlgDesign", repos="https://cran.rstudio.com")'
 pip install dataset
@@ -15,6 +10,11 @@ cd $APP_TARGET
 
 #./run_multiple.sh 1 stencil3d.src1_linear.c
 ./run_multiple.sh 1 stencil3d.src1_random.c
+
+USR="pbruel"
+APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/stencil3d"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/stencil3d"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
