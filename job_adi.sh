5160dbe5638411861e84a2857b653b1b979c99a2
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-05 20:24:31 +0200

    Wed Sep  5 20:24:31 CEST 2018

---
+++ job_adi.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+apt-get install -y python-rpy2 r-cran-car
+Rscript -e 'install.packages("AlgDesign", repos="https://cran.rstudio.com")'
+pip install dataset
+
+CLONE_TARGET="/root/dlmt_spapt_experiments"
+
+echo "Updating target data directory"
+
+if [ -d "$CLONE_TARGET" ]; then
+    git -C ${CLONE_TARGET} pull
+else
+    git clone https://github.com/phrb/dlmt_spapt_experiments.git
+fi
+
+APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/adi"
+cd $APP_TARGET
+
+./run_multiple.sh 1 adi.ancc.src1.c
+
+USR="pbruel"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/adi"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+
+cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+
+su ${USR} -c "mkdir -p ${USR_TARGET}"
+su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+
+./run_multiple.sh 1 adi.ancc.src1_random.c
+
+USR="pbruel"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/adi"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+
+cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+
+su ${USR} -c "mkdir -p ${USR_TARGET}"
+su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+
+./run_multiple.sh 1 adi.ancc.src1_sima.c
+
+USR="pbruel"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/adi"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+
+cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+
+su ${USR} -c "mkdir -p ${USR_TARGET}"
+su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
--- job_atax.sh
+++ job_atax.sh
@@ -9,7 +9,18 @@ git clone https://github.com/phrb/dlmt_spapt_experiments.git
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/atax"
 cd $APP_TARGET
 
-./run_multiple.sh 1 atax2.src0.c
+#./run_multiple.sh 1 atax2.src0.c
+#
+#USR="pbruel"
+#USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
+#NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+#
+#cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+#
+#su ${USR} -c "mkdir -p ${USR_TARGET}"
+#su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+
+./run_multiple.sh 1 atax2.src0_random.c
 
 USR="pbruel"
 USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
@@ -20,24 +31,13 @@ cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
 su ${USR} -c "mkdir -p ${USR_TARGET}"
 su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
-#./run_multiple.sh 1 atax2.src0_random.c
-#
-#USR="pbruel"
-#USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
-#NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-#
-#cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
-#
-#su ${USR} -c "mkdir -p ${USR_TARGET}"
-#su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
-#
-#./run_multiple.sh 1 atax2.src0_sima.c
-#
-#USR="pbruel"
-#USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
-#NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-#
-#cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
-#
-#su ${USR} -c "mkdir -p ${USR_TARGET}"
-#su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+./run_multiple.sh 1 atax2.src0_sima.c
+
+USR="pbruel"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+
+cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+
+su ${USR} -c "mkdir -p ${USR_TARGET}"
+su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
