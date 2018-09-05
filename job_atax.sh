0c603d23c15e143eaf1c3ae80066a26995a21db2
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-05 17:44:18 +0200

    Wed Sep  5 17:44:18 CEST 2018

--- job_atax.sh
+++ job_atax.sh
@@ -20,13 +20,24 @@ cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
 su ${USR} -c "mkdir -p ${USR_TARGET}"
 su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
-./run_multiple.sh 1 atax2.src0_random.c
-
-USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
-NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-
-cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
-
-su ${USR} -c "mkdir -p ${USR_TARGET}"
-su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+#./run_multiple.sh 1 atax2.src0_random.c
+#
+#USR="pbruel"
+#USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
+#NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+#
+#cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+#
+#su ${USR} -c "mkdir -p ${USR_TARGET}"
+#su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+#
+#./run_multiple.sh 1 atax2.src0_sima.c
+#
+#USR="pbruel"
+#USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax"
+#NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+#
+#cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+#
+#su ${USR} -c "mkdir -p ${USR_TARGET}"
+#su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
