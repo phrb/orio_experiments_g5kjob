ed5df09017460806d7171d015508e83608a804a4
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-12 19:08:35 +0200

    Wed Sep 12 19:08:35 CEST 2018

--- job_adi.sh
+++ job_adi.sh
@@ -14,38 +14,56 @@ else
     git clone https://github.com/phrb/dlmt_spapt_experiments.git
 fi
 
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/adi/adi1_cubic_quad_big_model"
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/adi"
 cd $APP_TARGET
 
-./run_multiple.sh 1 adi.ancc.src1.c
+# ./run_multiple.sh 1 adi.ancc.src1.c
+#
+# USR="pbruel"
+# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+#
+# mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+#
+# su ${USR} -c "mkdir -p ${USR_TARGET}"
+# su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
+
+./run_multiple.sh 1 adi.ancc.src1_random.c
 
 USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/adi/adi1_cubic_quad_big_model"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
-mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
 su ${USR} -c "mkdir -p ${USR_TARGET}"
-su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
+su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
-#./run_multiple.sh 1 adi.ancc.src1_random.c
+./run_multiple.sh 1 adi.ancc.src1_sima.c
+
+USR="pbruel"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+
+cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+
+su ${USR} -c "mkdir -p ${USR_TARGET}"
+su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+
+# ./run_multiple.sh 1 adi.ancc.src1_rsl.c
 #
-#USR="pbruel"
-#USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/adi"
-#NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+# USR="pbruel"
+# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 #
-#cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+# cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
 #
-#su ${USR} -c "mkdir -p ${USR_TARGET}"
-#su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+# su ${USR} -c "mkdir -p ${USR_TARGET}"
+# su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
 #
-#./run_multiple.sh 1 adi.ancc.src1_sima.c
+# ./run_multiple.sh 1 adi.ancc.src1_simp.c
 #
-#USR="pbruel"
-#USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/adi"
-#NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+# USR="pbruel"
+# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 #
-#cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+# cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
 #
-#su ${USR} -c "mkdir -p ${USR_TARGET}"
-#su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+# su ${USR} -c "mkdir -p ${USR_TARGET}"
+# su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
