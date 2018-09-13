b18032242da48e0d94f6192498564f75584df52e
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-13 13:50:41 +0200

    Thu Sep 13 13:50:41 CEST 2018

--- job_adi.sh
+++ job_adi.sh
@@ -14,14 +14,14 @@ else
     git clone https://github.com/phrb/dlmt_spapt_experiments.git
 fi
 
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/adi/adi1_cubic_quad_big_model"
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/adi"
 cd $APP_TARGET
 
+USR="pbruel"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/adi/adi1_cubic_quad_big_model"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+
 # ./run_multiple.sh 1 adi.ancc.src1.c
-#
-# USR="pbruel"
-# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 #
 # mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 #
@@ -30,40 +30,28 @@ cd $APP_TARGET
 
 ./run_multiple.sh 1 adi.ancc.src1_random.c
 
-USR="pbruel"
-NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-
-cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
 su ${USR} -c "mkdir -p ${USR_TARGET}"
-su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
 ./run_multiple.sh 1 adi.ancc.src1_sima.c
 
-USR="pbruel"
-NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-
-cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
 su ${USR} -c "mkdir -p ${USR_TARGET}"
-su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
 # ./run_multiple.sh 1 adi.ancc.src1_rsl.c
-#
-# USR="pbruel"
-# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-#
-# cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
-#
+# 
+# mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+# 
 # su ${USR} -c "mkdir -p ${USR_TARGET}"
-# su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
-#
+# su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
+# 
 # ./run_multiple.sh 1 adi.ancc.src1_simp.c
-#
-# USR="pbruel"
-# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-#
-# cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
-#
+# 
+# mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+# 
 # su ${USR} -c "mkdir -p ${USR_TARGET}"
-# su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+# su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
