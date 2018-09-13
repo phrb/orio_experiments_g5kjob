2cb50bf4c86ddce24528d13ca1720ee7d2b7a026
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-13 18:16:28 +0200

    Thu Sep 13 18:16:28 CEST 2018

--- job_atax.sh
+++ job_atax.sh
@@ -14,36 +14,28 @@ else
     git clone https://github.com/phrb/dlmt_spapt_experiments.git
 fi
 
+USR="pbruel"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/atax/atax_4x_lin"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/atax"
 cd $APP_TARGET
 
-./run_multiple.sh 1 atax2.src1.c
-
-USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax_quad_cubic"
-NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+./run_multiple.sh 1 atax2.c
 
 mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
 su ${USR} -c "mkdir -p ${USR_TARGET}"
 su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
-# ./run_multiple.sh 2 atax2.src1_random.c
-#
-# USR="pbruel"
-# USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax1_2_steps"
-# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-#
-# mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
-#
-# su ${USR} -c "mkdir -p ${USR_TARGET}"
-# su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
-#
+./run_multiple.sh 1 atax2_random.c
+
+mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+
+su ${USR} -c "mkdir -p ${USR_TARGET}"
+su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
+
 # ./run_multiple.sh 2 atax2.src1_sima.c
-#
-# USR="pbruel"
-# USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax1_2_steps"
-# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 #
 # mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 #
@@ -52,20 +44,12 @@ su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 #
 # ./run_multiple.sh 2 atax2.src1_rsl.c
 #
-# USR="pbruel"
-# USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax1_2_steps"
-# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
-#
 # mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 #
 # su ${USR} -c "mkdir -p ${USR_TARGET}"
 # su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 #
 # ./run_multiple.sh 2 atax2.src1_simp.c
-#
-# USR="pbruel"
-# USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/atax1_2_steps"
-# NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 #
 # mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 #
