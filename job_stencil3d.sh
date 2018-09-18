26795db0ea14b1aaf4036f2a8c5be9859436f3a5
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-18 19:03:56 +0200

    Tue Sep 18 19:03:56 CEST 2018

--- job_stencil3d.sh
+++ job_stencil3d.sh
@@ -18,7 +18,7 @@ APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/stencil3d"
 cd $APP_TARGET
 
 USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/stencil3d/stencil3d_3x_lin_quad_cub"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/stencil3d/stencil3d_3x_1_step_lin_quad"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 ./run_multiple.sh 1 stencil3d.c
@@ -27,7 +27,7 @@ mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
 su ${USR} -c "mkdir -p ${USR_TARGET}"
 su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
-#
+
 #./run_multiple.sh 1 stencil3d_random.c
 #
 #cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
