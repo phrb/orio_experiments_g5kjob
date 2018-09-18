a90e06d895e0a008985aab8e459942a569d3c1fc
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-18 19:09:55 +0200

    Tue Sep 18 19:09:55 CEST 2018

--- job_atax.sh
+++ job_atax.sh
@@ -15,20 +15,20 @@ else
 fi
 
 USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/atax/atax_1_step_4x_lin_quad_no_extra"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/atax/atax_1_step_4x_lin_quad_no_extra_no_constraints"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/atax"
 cd $APP_TARGET
 
-./run_multiple.sh 2 atax2.c
+./run_multiple.sh 1 atax2.c
 
 mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
 su ${USR} -c "mkdir -p ${USR_TARGET}"
 su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
-./run_multiple.sh 2 atax2_random.c
+./run_multiple.sh 1 atax2_random.c
 
 mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
