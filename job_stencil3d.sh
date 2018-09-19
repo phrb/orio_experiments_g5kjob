e0f3b57cf0c17c2f326796902c1ff9d9a00edd8f
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-19 18:51:09 +0200

    Wed Sep 19 18:51:09 CEST 2018

--- job_stencil3d.sh
+++ job_stencil3d.sh
@@ -18,7 +18,7 @@ APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/stencil3d"
 cd $APP_TARGET
 
 USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/stencil3d/stencil3d_3x_1_step_lin_quad"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/stencil3d/stencil3d_3x_4_steps_lin"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 ./run_multiple.sh 1 stencil3d.c
@@ -28,9 +28,9 @@ mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 su ${USR} -c "mkdir -p ${USR_TARGET}"
 su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
-./run_multiple.sh 1 stencil3d_random.c
-
-cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
-
-su ${USR} -c "mkdir -p ${USR_TARGET}"
-su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
+#./run_multiple.sh 1 stencil3d_random.c
+#
+#cp -r ${APP_TARGET}/${NODE_NAME}_* /tmp/
+#
+#su ${USR} -c "mkdir -p ${USR_TARGET}"
+#su ${USR} -c "cp -r /tmp/${NODE_NAME}_* ${USR_TARGET}"
