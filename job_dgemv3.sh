44e4155c8ceaa8be7a450f8cd4b018df264a4193
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-19 20:48:43 +0200

    Wed Sep 19 20:48:43 CEST 2018

--- job_dgemv3.sh
+++ job_dgemv3.sh
@@ -15,7 +15,7 @@ else
 fi
 
 USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/dgemv3/dgemv3_4_steps_lin_quad"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/dgemv3/dgemv3_4_steps_lin_quad_true"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/dgemv3"
