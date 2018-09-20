5358480ae435e6082e822a7f4063e4a5454b7ae6
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-20 19:36:57 +0200

    Thu Sep 20 19:36:57 CEST 2018

--- adi.sh
+++ adi.sh
@@ -11,14 +11,14 @@ echo "Updating target data directory"
 if [ -d "$CLONE_TARGET" ]; then
     git -C ${CLONE_TARGET} pull
 else
-    git clone https://github.com/phrb/dlmt_spapt_experiments.git
+    git clone --depth 1 https://github.com/phrb/dlmt_spapt_experiments.git
 fi
 
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/adi"
 cd $APP_TARGET
 
 USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/adi/adi_3x_cubic_quad_big_model"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/results/adi"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 ./run_multiple.sh 1 adi.c
@@ -28,12 +28,12 @@ mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 su ${USR} -c "mkdir -p ${USR_TARGET}"
 su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
-#./run_multiple.sh 1 adi_random.c
-#
-#mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
-#
-#su ${USR} -c "mkdir -p ${USR_TARGET}"
-#su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
+./run_multiple.sh 1 adi_random.c
+
+mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+
+su ${USR} -c "mkdir -p ${USR_TARGET}"
+su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 #
 #./run_multiple.sh 1 adi.ancc.src1_sima.c
 #
