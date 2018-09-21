ed23b0d9339701a6fd720324356030a1adedaffd
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-21 13:51:52 +0200

    Fri Sep 21 13:51:52 CEST 2018

--- allocate_resources.sh
+++ allocate_resources.sh
@@ -15,5 +15,5 @@ fi
 
 echo "Allocating machines"
 
-oarsub -q default -p "cluster='grisou'" -l nodes=10,walltime=14 -t deploy -r "2018-09-21 19:00:00"
+oarsub -q default -p "cluster='grimoire'" -l nodes=8,walltime=60 -t deploy -r "2018-09-21 19:00:00"
 #oarsub -I -q default -p "cluster='paravance'" -l nodes=10,walltime=12 -t deploy
