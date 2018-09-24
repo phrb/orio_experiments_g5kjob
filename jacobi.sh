b8d700e641b699b043320f067d794185ffbb48aa
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-24 16:59:39 +0200

    Mon Sep 24 16:59:39 CEST 2018

---
+++ jacobi.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+apt-get install -y python-rpy2 r-cran-car
+Rscript -e 'install.packages("AlgDesign", repos="https://cran.rstudio.com")'
+pip install dataset
+
+CLONE_TARGET="/root/dlmt_spapt_experiments"
+
+echo "Updating target data directory"
+
+if [ -d "$CLONE_TARGET" ]; then
+    git -C ${CLONE_TARGET} pull
+else
+    git clone --depth 1 https://github.com/phrb/dlmt_spapt_experiments.git
+fi
+
+USR="pbruel"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/results/jacobi"
+NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
+
+APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/jacobi"
+cd $APP_TARGET
+
+./run_multiple.sh 1 jacobi-1d.c
+
+mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+
+su ${USR} -c "mkdir -p ${USR_TARGET}"
+su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
+
+./run_multiple.sh 1 jacobi-1d_random.c
+
+mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+
+su ${USR} -c "mkdir -p ${USR_TARGET}"
+su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
