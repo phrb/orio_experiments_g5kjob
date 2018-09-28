bf1cad8d6ab33320240fa4d4ecd9e8a5c963c1d7
Pedro Bruel <pedro.bruel@gmail.com>
2018-09-28 14:16:48 +0200

    Fri Sep 28 14:16:48 CEST 2018

--- adi.sh
+++ adi.sh
@@ -21,12 +21,12 @@ USR="pbruel"
 USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/results/adi"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
-./run_multiple.sh 1 adi.c
-
-mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
-
-su ${USR} -c "mkdir -p ${USR_TARGET}"
-su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
+#./run_multiple.sh 1 adi.c
+#
+#mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+#
+#su ${USR} -c "mkdir -p ${USR_TARGET}"
+#su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
 
 ./run_multiple.sh 1 adi_random.c
 
