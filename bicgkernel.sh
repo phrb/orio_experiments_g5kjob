df53dc6591b56d8bd67ada23ab1e69dbe4193e18
Pedro Bruel <pedro.bruel@gmail.com>
2019-07-23 16:32:36 -0300

    Prepare larger experiments with quad terms

--- bicgkernel.sh
+++ bicgkernel.sh
@@ -15,22 +15,22 @@ else
 fi
 
 USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/random_search/bicgkernel"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/threshold_0.01_16x_nocube/bicgkernel"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/bicgkernel"
 cd $APP_TARGET
 
-#./run_multiple.sh 1 bicgkernel.c
-#
-#mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
-#
-#su ${USR} -c "mkdir -p ${USR_TARGET}"
-#su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
-
-./run_multiple.sh 1 bicgkernel_random.c
+./run_multiple.sh 1 bicgkernel.c
 
 mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
 su ${USR} -c "mkdir -p ${USR_TARGET}"
 su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
+
+#./run_multiple.sh 1 bicgkernel_random.c
+#
+#mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
+#
+#su ${USR} -c "mkdir -p ${USR_TARGET}"
+#su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
