6ee453a14416c2d230da42f84707cd81b5101de7
Pedro Bruel <pedro.bruel@gmail.com>
2019-09-21 05:30:57 +0200

    Update allocate for nancy

--- allocate_resources.sh
+++ allocate_resources.sh
@@ -15,6 +15,9 @@ fi
 
 echo "Allocating machines"
 
-#oarsub -q default -p "cluster='grisou'" -l nodes=9,walltime=14 -t deploy -r "2018-09-21 19:00:00"
+# Nancy
+
+#oarsub -q default -p "cluster='grisou'" -l nodes=51,walltime=14 -t deploy -r "2018-10-04 19:00:00"
 #oarsub -I -q default -p "cluster='paravance'" -l nodes=10,walltime=12 -t deploy
-oarsub -I -q production -p "cluster='graoully'" -l nodes=10,walltime=48 -t deploy
+oarsub -I -q production -p "cluster='graoully'" -l nodes=10,walltime=8 -t deploy
+#oarsub -I -q default -p "cluster='grisou'" -l nodes=15,walltime=6 -t deploy
