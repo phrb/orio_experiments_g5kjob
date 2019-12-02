8f64d210018b580ad958f2dc8889754ee4aec233
Pedro Bruel <pedro.bruel@gmail.com>
2019-12-02 11:33:51 -0800

    Update hessian script

--- hessian.sh
+++ hessian.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 apt-get install -y python-rpy2 r-cran-car
-Rscript -e 'install.packages("AlgDesign", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages(c("AlgDesign", "rsm", "dplyr", "quantreg", "DiceKriging", "DiceDesign", "DiceOptim", "tibble", "randtoolbox", "stringr", "foreach", "doParallel", "rgenoud"), repos="https://cran.rstudio.com")'
 pip install dataset
 
 CLONE_TARGET="/root/dlmt_spapt_experiments"
@@ -15,20 +15,15 @@ else
 fi
 
 USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/results/hessian"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/gpr_hessian_v0/hessian"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/hessian"
 cd $APP_TARGET
 
-./run_multiple.sh 1 hessian.c
-
-mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
-
-su ${USR} -c "mkdir -p ${USR_TARGET}"
-su ${USR} -c "mv /tmp/${NODE_NAME}_* ${USR_TARGET}"
-
-./run_multiple.sh 1 hessian_random.c
+# ./run_multiple.sh 1 hessian_random.c
+# ./run_multiple.sh 1 hessian.c
+./run_multiple.sh 1 hessian_gpr.c
 
 mv ${APP_TARGET}/${NODE_NAME}_* /tmp/
 
