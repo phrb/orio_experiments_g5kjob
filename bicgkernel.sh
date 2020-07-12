3178acd0774efb01d459b84552dfeec79ff8e13c
Pedro Bruel <pedro.bruel@gmail.com>
2020-07-11 22:41:40 -0300

    Update bicgkernel script

--- bicgkernel.sh
+++ bicgkernel.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 apt-get install -y python-rpy2 r-cran-car
-Rscript -e 'install.packages(c("future.apply", "AlgDesign", "rsm", "dplyr", "quantreg", "DiceKriging", "DiceDesign", "DiceOptim", "tibble", "randtoolbox", "stringr", "foreach", "doParallel", "rgenoud"), repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("devtools", repos="https://cran.rstudio.com")'
+Rscript -e 'install_version("dplyr", version="0.8.5", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages(c("future.apply", "AlgDesign", "rsm", "quantreg", "DiceKriging", "DiceDesign", "DiceOptim", "tibble", "randtoolbox", "stringr", "foreach", "doParallel", "rgenoud"), repos="https://cran.rstudio.com")'
 pip install -Iv dataset==1.1.2
 
 CLONE_TARGET="/root/dlmt_spapt_experiments"
@@ -15,7 +17,7 @@ else
 fi
 
 USR="pbruel"
-USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/scaled_response/bicgkernel"
+USR_TARGET="/home/${USR}/dlmt_spapt_experiments/data/tests/gpr_expandaded_ss/bicgkernel"
 NODE_NAME="xeon_e5_2630_v3_$(uname -n | cut -d. -f1)"
 
 APP_TARGET="/root/dlmt_spapt_experiments/orio/testsuite/SPAPT/bicgkernel"
